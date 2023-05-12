b6deab0b8105e387743f898cbfa863cdf9032c9b
Péter Attila Rigó <61515182+rigopeti96@users.noreply.github.com>
2023-05-12 17:33:07 +0200

    network setup

--- app/src/main/java/hu/bme/aut/android/bggapplication/network/BoardGameGeekAPI.kt
+++ app/src/main/java/hu/bme/aut/android/bggapplication/network/BoardGameGeekAPI.kt
@@ -1,4 +1,15 @@
 package hu.bme.aut.android.bggapplication.network
 
+import hu.bme.aut.android.bggapplication.data.GameDetailsData
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface BoardGameGeekAPI {
+    @GET("boardgame/{id}")
+    fun getGameDetailsData(
+        @Path("id") id: Int,
+        @Body collectionPointPutData: GameDetailsData
+    ): Call<Any?>
 }
--- app/src/main/java/hu/bme/aut/android/bggapplication/network/NetworkManager.kt
+++ app/src/main/java/hu/bme/aut/android/bggapplication/network/NetworkManager.kt
@@ -1,4 +1,21 @@
 package hu.bme.aut.android.bggapplication.network
 
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object NetworkManager {
+    private val retrofit: Retrofit
+    private val boardGameGeekAPI: BoardGameGeekAPI
+
+    private const val SERVICE_URL = "https://boardgamegeek.com/xmlapi/"
+
+    init {
+        retrofit = Retrofit.Builder()
+            .baseUrl(SERVICE_URL)
+            .client(OkHttpClient.Builder().build())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        boardGameGeekAPI = retrofit.create(BoardGameGeekAPI::class.java)
+    }
 }
