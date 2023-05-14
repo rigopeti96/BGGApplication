b89c59ac1faf137eb5b78869663e5395e8dbdf74
Péter Attila Rigó <61515182+rigopeti96@users.noreply.github.com>
2023-05-14 21:06:11 +0200

    Query update

--- app/src/main/java/hu/bme/aut/android/bggapplication/network/BoardGameGeekAPI.kt
+++ app/src/main/java/hu/bme/aut/android/bggapplication/network/BoardGameGeekAPI.kt
@@ -6,23 +6,23 @@ import hu.bme.aut.android.bggapplication.data.SearchListByName
 import hu.bme.aut.android.bggapplication.data.SearchListItem
 import okhttp3.ResponseBody
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.Path
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface BoardGameGeekAPI {
     @GET("game/{id}")
+    @Headers("Content-Type: application/xml; charset=utf-8")
     fun getGameDetailsData(
         @Path("id") id: Int
     ): Call<SearchListByName?>
 
-    @GET("search/{name}")
+    @GET("search")
+    @Headers("Content-Type: application/xml; charset=utf-8")
     fun getGameList(
-        @Query("name") name: String
+        @Query("search") name: String
     ): Call<ResponseBody?>
 
     @GET("collection/{username}")
+    @Headers("Content-Type: application/xml; charset=utf-8")
     fun getCollectionList(
         @Path("username") username: String
     ): Call<ResponseBody?>
